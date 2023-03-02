17af59766044b0aa5ea90c8a9a84bc8f78836283
Naveena Pius <naveenapius@protonmail.com>
2023-03-02 09:18:59 +0530

    init

---
+++ gettingstarted.php
@@ -0,0 +1,8 @@
+<html>
+<head><title>My First PHP page</title></head>
+<body style="color:white; background-color:black;font-family:sans-serif;font-size:20;text-align:center;">
+<?php
+echo "Hello World";
+?>
+</body>
+</html>
